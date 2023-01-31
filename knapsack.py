349e1656a2eb4c568a5220017c7ebba533846bde
sa06840 <sa06840@st.habib.edu.pk>
2023-02-01 00:03:28 +0500

    Implemented new functions in knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -1,47 +1,104 @@
 import random
 
+class Knapsack():
+    
+    def __init__(self) -> None:
 
-# Knapsack Problem
+        self.populationSize = 30
+        self.population = [0]*self.populationSize
+        self.text = []
 
-def populate(text):
-    population = [0]*30
-    for i in range(30):
-        chromosome = [0]*text[0][0]
-        for j in range(text[0][0]):
-            chromosome[j] = random.randint(0,1)
-        population[i] = [0, chromosome]
-    return population
+        file = open('/Users/sajeelnadeemalam/Documents/CI/assignment_1/instances_01_KP/low-dimensional/f8_l-d_kp_23_10000', 'r')
+        read = file.readlines()
+        for line in read:
+            if line[-1] == '\n':
+                row = [int(i) for i in line[:-1].split()]
+                self.text.append(row)
+            else:
+                row = [int(i) for i in line.split()]
+                self.text.append(row)
 
-def readFile():
-    file = open('/Users/sajeelnadeemalam/Documents/CI/assignment_1/instances_01_KP/low-dimensional/f8_l-d_kp_23_10000', 'r')
-    read = file.readlines()
-    text = []
-    for line in read:
-        if line[-1] == '\n':
-            row = [int(i) for i in line[:-1].split()]
-            text.append(row)
+    def populate(self):
+        for i in range(30):
+            chromosome = [0] * self.text[0][0]
+            for j in range(self.text[0][0]):
+                chromosome[j] = random.randint(0,1)
+            self.population[i] = [0, chromosome]
+        return self.population
+
+    def calculateFitness(self):
+        for i in range(len(self.population)):
+            totalWeight = 0
+            totalProfit = 0
+            for j in range(len(self.population[0][1])):
+                if self.population[i][1][j] != 0:
+                    totalWeight += self.text[j+1][1]
+                    totalProfit += self.text[j+1][0]
+                self.population[i][0] = totalProfit
+                if totalWeight > self.text[0][1]:
+                    self.population[i][0] = 0
+        return self.population
+
+    def selectionRandom(self):
+        p1Index = random.randint(0,self.populationSize-1)
+        p2Index = random.randint(0,self.populationSize-1)
+        while p1Index == p2Index:
+            p2Index = random.randint(0,self.populationSize-1)
+        p1 = self.population[p1Index]
+        p2 = self.population[p2Index]
+        return [p1, p2]
+
+    def crossover(self, p1, p2):
+        offspring1 = offspring2 = [0]*self.text[0][0]
+        p1 = p1[1]
+        p2 = p2[1]
+        start = random.randint(0, len(p1)//2)
+        end = random.randint(len(p2)//2 + 1, len(p2)-1)
+
+        offspring1[start:end+1] = p1[start:end+1]
+        offspring1[0:start] = p2[0:start]
+        offspring1[end+1:len(p1)] = p2[end+1:len(p1)]
+
+        offspring2[start:end+1] = p2[start:end+1]
+        offspring2[0:start] = p1[0:start]
+        offspring2[end+1:len(p1)] = p1[end+1:len(p1)]
+        
+    def mutation(self, offspring):
+        randomIndex1 = random.randint(0,self.text[0][0]-1)
+        randomIndex2 = random.randint(0,self.text[0][0]-1)
+        while randomIndex1 == randomIndex2:
+            randomIndex2 = random.randint(0,self.text[0][0]-1)
+        if offspring[randomIndex1] == 1:
+            offspring[randomIndex1] = 0
+            if offspring[randomIndex2] == 1:
+                offspring[randomIndex2] = 0
+            else:
+                offspring[randomIndex2] = 1
         else:
-            row = [int(i) for i in line.split()]
-            text.append(row)
-    print(text)
-    return text
+            offspring[randomIndex1] = 1
+            if offspring[randomIndex2] == 1:
+                offspring[randomIndex2] = 0
+            else:
+                offspring[randomIndex2] = 1
 
-def calculateFitness(population, text):
-    for i in range(len(population)):
+    def newFitness(self, offspring):
         totalWeight = 0
         totalProfit = 0
-        for j in range(len(population[0][1])):
-            if population[i][1][j] != 0:
-                totalWeight += text[j+1][1]
-                totalProfit += text[j+1][0]
-            population[i][0] = totalProfit
-            if totalWeight > text[0][1]:
-                population[i][0] = 0
-    return population
+        for i in range(len(offspring)):
+            if offspring[i] != 0:
+                totalWeight += self.text[i+1][1]
+                totalProfit += self.text[i+1][0]
+            offspring.insert(0, totalProfit)
+            if totalWeight > 10000:
+                offspring[0] = 0
+        self.population.append(offspring)
 
-text = readFile()
-population = populate(text)
-print(calculateFitness(population, text))
+    def fitnessSurvivor(self):
+        self.population.sort()
+        self.population.reverse()
+        self.population = self.population[0:self.populationSize]
+
+
 
 
 
--- tsp.py
+++ tsp.py
@@ -1,1 +1,11 @@
-print("hello")
+import numpy as np
+import tsp
+import tsplib95
+
+problem = tsplib95.load('qa194.tsp')
+
+#print(problem)
+
+temp = problem.as_name_dict()
+res = temp["node_coords"]
+print(res)
