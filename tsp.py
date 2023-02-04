dd2474aa9e5411c585e02e9aa0d7128b2b948ff7
muneebshafique <64555362+muneebshafique@users.noreply.github.com>
2023-02-04 16:16:58 +0500

    Update tsp.py

--- tsp.py
+++ tsp.py
@@ -2,7 +2,8 @@ import tsplib95
 import math
 import random
 import copy
-
+from matplotlib import pyplot as plt
+from operator import add
 
 
 
@@ -15,6 +16,13 @@ class TSP():
         self.population=[]                  #stores all solutions
         self.listOfCountries=[]             #stores names of countries
         self.fitness=[]                     #stores total distance of every solution
+        self.bestFitness= []
+        self.averageFitness =[]
+        self.numOfGenerations = 100
+        self.numOfIterations  = 10
+        
+        
+        self.bestsofar = 9999999999
 
         problem = tsplib95.load(filename)
         temp = problem.as_name_dict()
@@ -42,12 +50,10 @@ class TSP():
             random.shuffle(solution)
             self.population.append([0,solution])
         
-        self.calculateFitness(self.population)
-        
-    #Calculating Total Distance of each solution and storing in Fitness list
-    def calculateFitness(self, Population):
+    #Calculating Total Distance of each solution 
+    def calculateFitness(self):
         count=0
-        for solution in Population:
+        for solution in self.population:
             totalDistance=0
             for i in range(len(solution[1])-1):
                 listOfDistance=self.euclideanDistance[solution[1][i]]
@@ -62,6 +68,27 @@ class TSP():
             self.population[count][0]=round(totalDistance,2)
             count+=1
 
+    def newFitness(self, offspring):
+        totalDistance=0
+        distance = 0
+
+        for i in range(len(offspring[1])-1):
+            listOfDistance=self.euclideanDistance[offspring[1][i]]
+            distance=listOfDistance[(offspring[1][i+1])-1]
+            totalDistance+=distance
+
+        '''
+        Adding Distance of going back to origin country
+        '''
+        listOfDistance=self.euclideanDistance[offspring[1][i+1]]
+        distance=listOfDistance[offspring[1][0]-1]
+        totalDistance+=distance
+        offspring[0]=round(totalDistance,2)
+
+        # self.population.append(offspring)
+        return (offspring)
+
+#Parent Selection Schemes
     def selectionRandom(self):
         p1Index = random.randint(0,self.populationSize-1)
         p2Index = random.randint(0,self.populationSize-1)
@@ -70,7 +97,11 @@ class TSP():
         p1 = self.population[p1Index]
         p2 = self.population[p2Index]
         return ([p1, p2])
-     
+
+    def selectionTruncation(self):
+        self.population.sort(key = lambda x: x[0])
+        return ([self.population[0], self.population[1]])
+
     def crossover(self, p1, p2):
         self.length_of_solution=len(self.listOfCountries)
         offspring1 = [0]*self.length_of_solution
@@ -108,6 +139,7 @@ class TSP():
         return [offspring1, offspring2]
 
     def mutation(self, offspring):
+
         randomIndex1 = random.randint(0,self.length_of_solution-1)
         randomIndex2 = random.randint(0,self.length_of_solution-1)
         while randomIndex1 == randomIndex2:
@@ -119,42 +151,254 @@ class TSP():
         offspring[1][randomIndex2] = country1
 
         return offspring
-        
-    def newFitness(self, offspring):
-        totalDistance=0
-        distance = 0
-
-        for i in range(len(offspring[1])-1):
-            listOfDistance=self.euclideanDistance[offspring[1][i]]
-            distance=listOfDistance[(offspring[1][i+1])-1]
-            totalDistance+=distance
-
-        '''
-        Adding Distance of going back to origin country
-        '''
-        listOfDistance=self.euclideanDistance[offspring[1][i+1]]
-        distance=listOfDistance[offspring[1][0]-1]
-        totalDistance+=distance
-        offspring[0]=round(totalDistance,2)
-
-        self.population.append(offspring)
-        return (offspring)
     
+    def fitnessProportional(self):
+
+        sumFitness = 0
+        normalizedFitness = []
+        ranges = []
+
+        for chromosome in self.population:
+            sumFitness += chromosome[0]
+        for chromosome in self.population:
+            normalizedFitness.append(chromosome[0]/sumFitness)
+
+        # print(normalizedFitness)
+        
+        pointer = 0
+        for i in range(len(normalizedFitness)):
+            limits = [pointer, pointer+normalizedFitness[i]]
+            ranges.append(limits)
+            pointer += normalizedFitness[i]
+
+        # print(ranges)
+
+        randomIndex = random.uniform(0,1)  # print(randomIndex)
+        for index in range(len(ranges)):
+            if randomIndex >= ranges[index][0] and randomIndex < ranges[index][1]:
+                p1Index = index
+            
+        p2Index = p1Index
+        while(p1Index == p2Index):
+            randomIndex = random.uniform(0,1)
+            for index in range(len(ranges)):
+                if randomIndex >= ranges[index][0] and randomIndex < ranges[index][1]:
+                    p2Index = index
+
+        return [self.population[p1Index], self.population[p2Index]]
+
+    def rankBased(self):
+
+        self.population.sort()
+        ranks = []
+        normalizedRanks = []
+        sumRanks = 0
+        ranges = []
+
+        for rank in range(1, len(self.population)+1):
+            ranks.append(rank)
+            sumRanks += rank
+        # print(ranks)
+        # print(sumRanks)
+        # self.population.reverse()
+        ranks.reverse()
+        for i in ranks:
+            normalizedRanks.append(i/sumRanks)
+
+        pointer = 0
+        for i in range(len(normalizedRanks)):
+            limits = [pointer, pointer+normalizedRanks[i]]
+            ranges.append(limits)
+            pointer += normalizedRanks[i]
+
+        print(ranges)
+        
+        randomIndex = random.uniform(0,1)
+        for index in range(len(ranges)):
+            if randomIndex >= ranges[index][0] and randomIndex < ranges[index][1]:
+                p1Index = index
+            
+        p2Index = p1Index
+        while(p1Index == p2Index):
+            randomIndex = random.uniform(0,1)
+            for index in range(len(ranges)):
+                if randomIndex >= ranges[index][0] and randomIndex < ranges[index][1]:
+                    p2Index = index
+
+        return [self.population[p1Index], self.population[p2Index]]
+
+    def parentBinary(self):
+        contestant1 = random.randint(0, self.populationSize-1)
+        contestant2 = random.choice(list(set(range(0, self.populationSize)) - set([contestant1])))
+
+        if self.population[contestant1][0] <= self.population[contestant2][0]:
+            p1Index = contestant1
+        else:
+            p1Index = contestant2
+
+        contestant1 = random.choice(list(set(range(0, self.populationSize)) - set([p1Index])))
+        contestant2 = random.choice(list(set(range(0, self.populationSize)) - set([p1Index, contestant1])))
+
+        if self.population[contestant1][0] <= self.population[contestant2][0]:
+            p2Index = contestant1
+        else:
+            p2Index = contestant2
+
+        return [self.population[p1Index], self.population[p2Index]]
+
+#Survivor selection schemes
     def truncation(self):
         self.population.sort(key = lambda x: x[0])
         self.population = self.population[0:30]
        
+    def random(self):
+        randomlist = random.sample(range(0, 40), 30)
+        temp_population = []
+        for index in randomlist:
+            temp_population.append(self.population[index])
+        
+        self.population=temp_population
 
+    def survivorBinary(self):
+        selectedIndexes = []
 
-filename ='qa194.tsp'
-T1=TSP(filename)
-parents = T1.selectionRandom()
-p1 = parents[0]
-p2 = parents[1]
-offsprings = T1.crossover(p1, p2)
-offspring1=T1.mutation(offsprings[0])
-offspring2=T1.mutation(offsprings[1])
-fitness_offspring1=T1.newFitness(offspring1)
-fitness_offspring2=T1.newFitness(offspring2)
-# print(fitness_offspring)
-T1.truncation()
+        for i in range(self.populationSize):
+            contestant1 = random.choice(list(set(range(0, len(self.population))) - set(selectedIndexes)))
+            contestant2 = random.choice(list(set(range(0, len(self.population))) - set(selectedIndexes + [contestant1])))
+
+            if self.population[contestant1][0] <= self.population[contestant2][0]:
+                selectedIndexes.append(contestant1)
+            else:
+                selectedIndexes.append(contestant2)
+
+        tempPopulation = []
+        for index in selectedIndexes:
+            tempPopulation.append(self.population[index])
+        
+        self.population = tempPopulation
+
+    def generationEvaluation(self):
+        totalDistance = 0
+        
+        for chromosome in self.population:
+            totalDistance += chromosome[0]
+            if chromosome[0] < self.bestsofar:
+                self.bestsofar = chromosome[0]
+
+        self.averageFitness.append(totalDistance/len(self.population))
+        self.bestFitness.append(self.bestsofar)
+
+    def iterationEvaluation(self, fitnessEvaluation,iteration):
+        # print(iteration)
+        if iteration not in fitnessEvaluation:
+            fitnessEvaluation[iteration] = [[],[]]
+            fitnessEvaluation[iteration][0] = self.averageFitness 
+            fitnessEvaluation[iteration][1] = self.bestFitness
+          
+    def plotGraphs(self, fitnessEvaluation):
+        x_axis_generations = []
+        addedAverageFitness = [0]*self.numOfGenerations
+        addedBestFitness = [0]*self.numOfGenerations
+        avgAverageFitness = []
+        avgBestFitness = []
+
+        # list representing x axis (num of Generations)
+        for i in range (1, self.numOfGenerations+1):
+            x_axis_generations.append(i)
+       
+       #adding avgaveragefitness and best fitness values across all iterations
+        for iteration in (fitnessEvaluation):
+            addedAverageFitness = list(map(add, fitnessEvaluation[iteration][0], addedAverageFitness))
+            addedBestFitness  = list(map(add, fitnessEvaluation[iteration][1], addedBestFitness ))
+        
+        #adjusting added avgavergaefitness and best fitness values
+        #creating list representing y_axis (average avergae fitness values) and (average average best fitness values)
+        for fitness in (addedAverageFitness):
+            avgAverageFitness.append(fitness/self.numOfIterations)
+        
+        for fitness in (addedBestFitness):
+            avgBestFitness.append(fitness/self.numOfIterations)
+              
+        plt.plot(x_axis_generations, avgAverageFitness, label = "Average Fitness")
+        plt.plot(x_axis_generations, avgBestFitness, label = "Best Fitness")
+        
+        plt.legend()
+        plt.show()
+
+# Evolutionary Algorithm
+def evolutionaryAlgorithm():
+    fitnessEvaluation = dict()
+    for iteration in range(1, 11):
+
+        # print("***** Iteration Number = " + str(iteration+1) + " *****")
+        filename ='qa194.tsp'
+        T1=TSP(filename)
+        # T1.populate()
+        T1.calculateFitness()
+
+        for generation in range(T1.numOfGenerations):
+            # print("***** Iteration Number = " + str(iteration+1) + ", Generation Number = " + str(generation+1) + " *****")
+
+            for i in range(5):
+                parents = T1.selectionRandom()
+                # print("PARENTS")
+                # print(T1.population)
+                # print("SELECTED PARENTS")
+                # parents = T1.selectionTruncation()
+                # parents = T1.fitnessProportional()
+                # parents = T1.rankBased()
+                # parents = T1.parentBinary()
+                
+                # print("PARENT 1")
+                # print(parents)
+                p1 = parents[0]
+                # print(p1)
+                # print("PARENT 2")
+                p2 = parents[1]
+                # print(p2)
+                
+                # print("OFF SPRINGSS")
+                offsprings = T1.crossover(p1, p2)
+                # print(offsprings)
+
+                for j in range(2):
+                    randomNumber = random.randint(0,1)
+                    if randomNumber == 1:
+                        tempOffspring = T1.mutation(offsprings[j])
+                        # print("MUTATED OFFSPRING")
+                        # print(tempOffspring)
+                        offsprings[j] = tempOffspring
+
+                    offspring = T1.newFitness(offsprings[j])
+                    # print("OFFSPRING WITH FITNESS VALUE")
+                    # print(offspring)
+                    T1.population.append(offspring)
+                    # print(len(T1.population))
+
+            T1.truncation()
+            T1.generationEvaluation()
+            # T1.survivorBinary()
+            # T1.random()
+            # T1.fitnessProportional()
+            # print(T1.population[0])
+        # print(T1.avergeFitness)
+        # print(T1.bestFitness)
+        T1.iterationEvaluation(fitnessEvaluation,iteration)
+        # print(len(T1.bestFitness))
+        # print(len(T1.avergeFitness))
+    # print(fitnessEvaluation)
+    T1.plotGraphs(fitnessEvaluation)
+
+evolutionaryAlgorithm()
+# filename ='qa194.tsp'
+# T1=TSP(filename)
+# parents = T1.selectionRandom()
+# p1 = parents[0]
+# p2 = parents[1]
+# offsprings = T1.crossover(p1, p2)
+# offspring1=T1.mutation(offsprings[0])
+# offspring2=T1.mutation(offsprings[1])
+# fitness_offspring1=T1.newFitness(offspring1)
+# fitness_offspring2=T1.newFitness(offspring2)
+# # print(fitness_offspring)
+# T1.truncation()
